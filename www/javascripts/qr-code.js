a9843793206c72e017f8091a9607b561a45fc6b5
Andre Kristensen <andre.kristensen@lyse.net>
2012-04-26 21:44:21 +0200

    fixed some stuff

--- www/javascripts/qr-code.js
+++ www/javascripts/qr-code.js
@@ -1,59 +1,59 @@
 $('#scan-qr-page').live('pageinit', function() {
-	
-	var restClient = new RestHandler();
-
-//$(document).ready(function() {
-
-  $('#capture-qr-code').click(function() {
-   // navigator.camera.getPicture(uploadPhoto,
-    //    function(message) onFail,
-     //   { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI }
-      //  );
-
-
-//function uploadPhoto(imageURI) {
- //var options = new FileUploadOptions(); 
- //options.fileKey="file";
- //options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
- //options.mimeType="image/jpeg";
-
-// var ft = new FileTransfer();
-// ft.upload(imageURI, restClient.baseUrl + 'fadder/scan-qr-code/' + student.id, win, fail, options);
-                          
-                              navigator.camera.getPicture(onSuccess, onFail, { quality: 50, 
-                                                          destinationType: Camera.DestinationType.FILE_URI }); 
-                              
-                              function onSuccess(imageURI) {
-                              alert(immageURI);
-                              }
-                              
-                              function onFail(message) {
-                              alert('Failed because: ' + message);
-                              }
-                              
-}
-/*
-var win =  function(r) {
-	if(r.responseCode == 200){
-		 
-		 restClient.findRestricted('students/' + student.id, function(data, textStatus, jqXHR) {  
-			$.mobile.hidePageLoadingMsg();
-			if(jqXHR.status == '200'){   
-				student = data;
-				alert('Du er i gruppe: ' + student.fadderGroup.groupNumber);	
-			}else{
-				alert("Beklager, en feil skjedde");
-			}
-		},function(jqXHR, textStatus, errorThrown){
-			alert('Beklager, en feil oppsto: ' + textStatus + errorThrown);
-		});
-	}
-}
-
-function fail(error) {
-alert("An error has occurred: Code = " = error.code);
-}
-}
- */);
-  
-});
+                        
+                        var restClient = new RestHandler();
+                        
+                        $('#capture-qr-code').click(function() {
+                                                    navigator.camera.getPicture(uploadPhoto,
+                                                                                function(message) { showErr('Kamera funker ikke', null); },
+                                                                                
+                                                                                // Camera options
+                                                                                { quality:         50, 
+                                                                                destinationType: navigator.camera.DestinationType.FILE_URI }
+                                                                                );
+                                                    
+                                                    
+                                                    function uploadPhoto(imageURI) {
+                                                    $.mobile.showPageLoadingMsg();
+                                                    var options = new FileUploadOptions();
+                                                    options.fileKey = "file";
+                                                    options.fileName = imageURI.substr(imageURI.lastIndexOf('/')+1);
+                                                    options.mimeType = "image/jpeg";
+                                                    
+                                                    var ft = new FileTransfer();
+                                                    ft.upload(
+                                                              imageURI, restClient.baseUrl + 'fadder/scan-qr-code/' + student.id,
+                                                              function(res) {
+                                                              $.mobile.hidePageLoadingMsg();
+                                                              if(res.responseCode == 200) {
+                                                              getGroupStudentWasAddedTo();
+                                                              } else {
+                                                              showErr('Beklager, noe er galt med QR koden', null);
+                                                              }
+                                                              },
+                                                              function (error) {
+                                                              $.mobile.hidePageLoadingMsg();
+                                                              showErr('Greide ikke lese koden', null);
+                                                              },
+                                                              options);  
+                                                    }
+                                                    
+                                                    function getGroupStudentWasAddedTo() {
+                                                    restClient.findRestricted(
+                                                                              'students/' + student.id,
+                                                                              function(data, textStatus, jqXHR) {  
+                                                                              $.mobile.hidePageLoadingMsg();
+                                                                              if(jqXHR.status == '200'){
+                                                                              student = data;
+                                                                              showErr(
+                                                                                      'Du er i gruppe: ' + student.fadderGroup.groupNumber, null);  
+                                                                              history.back();
+                                                                              } else {  
+                                                                              showErr('Beklager, en feil skjedde', null);
+                                                                              }
+                                                                              },function(jqXHR, textStatus, errorThrown){
+                                                                              showErr('Beklager, en feil oppsto: ' + textStatus + errorThrown, null);
+                                                                              }
+                                                                              );
+                                                    }
+                                                    });
+                        });
