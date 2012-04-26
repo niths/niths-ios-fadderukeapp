427318ac232f3c447ea2b4e661f6ce5c3aa52e69
Andre Kristenen <andre.kristensen@lyse.net>
2012-04-26 17:25:06 +0200

    added some more fixes

--- www/javascripts/common/application.js
+++ www/javascripts/common/application.js
@@ -11,11 +11,20 @@ developerKey = 'sob8gvpyk2';
 
 htmlEncode = function(s) {
     encodedHtml = escape(s);
-    encodedHtml = encodedHtml.replace(/%20/g," ");
+    encodedHtml = encodedHtml.replace(/%20/g," "); // white space
+    encodedHtml = encodedHtml.replace(/%E5/g,"æ"); // æ
+    encodedHtml = encodedHtml.replace(/%F8/g,"ø"); // ø
+    encodedHtml = encodedHtml.replace(/%E6/g,"å"); // å
+    
+    encodedHtml = encodedHtml.replace(/%C6/g,"Æ"); // Æ
+    encodedHtml = encodedHtml.replace(/%D8/g,"Ø"); // Ø
+    encodedHtml = encodedHtml.replace(/%C5/g,"Å"); // Å
+    
+      
     encodedHtml = encodedHtml.replace(/\//g,"%2F");
     encodedHtml = encodedHtml.replace(/\?/g,"%3F");
     encodedHtml = encodedHtml.replace(/=/g,"%3D");
     encodedHtml = encodedHtml.replace(/&/g,"%26");
     encodedHtml = encodedHtml.replace(/@/g,"%40");
     return encodedHtml;
-}
+ }
