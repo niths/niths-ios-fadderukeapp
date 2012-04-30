e74c304d7f1c1f2e2bb878c3e80171c173a28b0c
Andre Kristensen <andre.kristensen@lyse.net>
2012-04-30 22:30:53 +0200

    added new dialog to delete students

--- www/javascripts/admin/fadder-children.js
+++ www/javascripts/admin/fadder-children.js
@@ -32,7 +32,6 @@ $("#fadderchildren-pagen").live('pageshow', function() {
     }
 
   $('#radio-delete').click(function() {
-//    $('#fadder-children-form').attr('action', 'confirm.html');
     $('#fadder-children-form').attr('action', '#confirm-dialog-page');
   });
 
@@ -61,12 +60,65 @@ $("#fadderchildren-pagen").live('pageshow', function() {
             /radio-method=/g, '');
 
         if (method == 'delete') {
-//          $.mobile.changePage('confirm.html', 'pop');
-          $.mobile.changePage('#confirm-dialog-page', 'pop');
+         // $.mobile.changePage('#confirm-dialog-page', 'pop');
+                                 ///----------
+                                 
+     
+         $('<div>').simpledialog2({
+         mode: 'button',
+            headerText: 'Slett event',
+    headerClose: true,
+        buttonPrompt: 'Er du sikker?',
+        buttons : {
+            'OK': {
+             click: function () { 
+               $('#buttonoutput').text('Ok');
+               deleteStudents();
+             }
+            },
+            'Avbryt': {
+              click: function () { 
+               $('#buttonoutput').text('Avbry');
+              },
+              icon: "delete",
+              theme: "c"
+            }
+        }
+        });
+    
+                      
         } else if (method == 'send-email') {
           $.mobile.changePage('#send-an-email-page');
         }
       }
     }
   });
+ function deleteStudents(){
+   // var restClient = new RestHandler();
+                                
+    var fadderChildren = JSON.parse(
+            sessionStorage.getItem('fadder_children_objs'));
+    var ids = '';
+                    
+    $.each(fadderChildren, function(i, fadderChild) {
+                    ids += fadderChild.id + ',';
+    });
+                            
+restClient.remove('fadder/' + sessionStorage.getItem('fadder_group_id') +
+        '/children/' + ids.slice(0, -1),  
+                  function(data, status, xhr) {  
+                        if(xhr.status== 200){
+                            alert('Sletting vellykket');
+                  
+                  $('#fadder-children-collection').empty();
+                  getOnlyFadderChildren();
+                        }else{
+                            alert(xhr.status + ': Sletting feilet');
+                        }
+                            $.mobile.hidePageLoadingMsg();
+                          
+                        });
+                        }                             
+                            
+                                
 });
