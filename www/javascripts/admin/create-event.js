8287cd8b03b6961ec223feb0ab2606724d41ce03
Andre Kristensen <andre.kristensen@lyse.net>
2012-05-03 11:43:41 +0200

    modified create event

--- www/javascripts/admin/create-event.js
+++ www/javascripts/admin/create-event.js
@@ -7,9 +7,8 @@ $("#admin-create-event-page").live('pageshow', function() {
                                    var dateToday = getDateTodayAsString();
                                    $('#startTime2').val(dateToday);
                                    $('#endTime2').val(dateToday);
-                                   $('#place2').val('Scheigaards gate 14');
-                                   
-                                   locationSearch('#place2','#create-event-location-selection');  
+                                   $('#place2').val('Schweigaards gate 14');
+                                   searchOnLocation();
                                    });
 
 function getDateTodayAsString(){
@@ -28,92 +27,86 @@ function getDateTodayAsString(){
     return today;
 }
 
-
-function locationSearch(inputField,dropDown){
- 
-    
+function searchOnLocation(){
     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' +
-         $('#place2').val() + '&sensor=true',
-          function(data) {
-          locations = data.results;
-          $.each(locations, function(key, location) {
-                 
-                 // Extract the country from the formatted address
-                 var address = location.formatted_address.split(/,/g);
-                 console.log('address '+ address);
-                 
-                 var country = $.trim(address[address.length - 1]);
-                 console.log('country ' + country);
-                 // Only include locations that are in Norway
-                 if (location.formatted_address != 'undefined'
-                     && (country == 'Norge' || country == 'Norway')) {
-                 $(dropDown).append('<option id="location-' +key +
-                                    '" value="' + location.formatted_address + '">' +
-                                    location.formatted_address +
-                                    '</option>');
-                  
-                 }
-                 });
-          });
-} 
+              $('#place2').val() + '&sensor=true',
+              function(data) {
+              locations = data.results;
+              $.each(locations, function(key, location) {
+                     
+                     // Extract the country from the formatted address
+                     var address = location.formatted_address.split(/,/g);
+                     var country = $.trim(address[address.length - 1]);
+                     
+                     // Only include locations that are in Norway
+                     if (location.formatted_address != 'undefined'
+                         && (country == 'Norge' || country == 'Norway')) {
+                     $('#create-event-location-selection').append(
+                                                                  '<option id="location-' +key +
+                                                                  '" value="' + location.formatted_address + '">' +
+                                                                  location.formatted_address +
+                                                                  '</option>');
+                     }
+                     });
+              });
+}
 
 $("#admin-create-event-page").live('pageinit', function() {
-                                         
-    var restClient = new RestHandler(); //REST CLIENT
-    var privacyDisplay = 
-    '<label for="select-privacy" class="select">Velg:</htmlEncode>'+
-            '<select name="select-privacy" id="select-privacy-choice2">'+
-            '<option value="public">Public</option>';
-        for (var i = 0; i < student.groupLeaders.length; i++){
-    privacyDisplay +=
-            '<option value="' + student.groupLeaders[i].groupNumber+'">' +
-        'For gruppe: ' + student.groupLeaders[i].groupNumber+
-            '</option>';
-                }
-    privacyDisplay += '</select>';
-            $('#privacy2').html(privacyDisplay);
-            $('#select-privacy-choice2').selectmenu();
-
-                                   // Handle location searching
-    $('#place2').keyup(function() {
-                       
-        $('#create-event-location-selection').empty();
-                       
-        $('#create-event-location-selection').append( '<option id="location+1'+
-                                                                    '" value="Velg en lokasjon">' +
-                                                                    'Velg en lokasjon' +
-                                                                    '</option>');
-        locationSearch('#place2','#create-event-location-selection');
-        }).keyup();
                                    
-            $('#create-event-location-selection').selectmenu();
-                           
-            $('#create-event-location-selection').change(function() {
-                // Extract the current index of the location
-                currentLocationIndex = /location-(\d+)/.exec(
-                $('#create-event-location-selection option:selected').attr('id'))[1];
-                    });
-                
-                $('#createeventsubmit').click(function() {
-                                                            
-                    restClient.create(
-                        'events/',
-                            getDataFromCreateForm(),
-                function(data, textStatus, jqXHR) {
-                    $.mobile.hidePageLoadingMsg();
-                    if (jqXHR.status == 201) {
-                    showMsg('Event opprettet', function() {
-                            history.go(-1);
-                                });
-                    } else {
-                        showErr('Noe gikk galt', null);
-                    }
-                    }
-        ); 
-
-    return false;
-    });
-});
+                                   var restClient = new RestHandler(); //REST CLIENT
+                                   var privacyDisplay = 
+                                   '<label for="select-privacy" class="select">Velg:</label>'+
+                                   '<select name="select-privacy" id="select-privacy-choice2">'+
+                                   '<option value="public">Public</option>';
+                                   for (var i = 0; i < student.groupLeaders.length; i++){
+                                   privacyDisplay +=
+                                   '<option value="' + student.groupLeaders[i].groupNumber+'">' +
+                                   'For gruppe: ' + student.groupLeaders[i].groupNumber+
+                                   '</option>';
+                                   }
+                                   privacyDisplay += '</select>';
+                                   $('#privacy2').html(privacyDisplay);
+                                   $('#select-privacy-choice2').selectmenu();
+                                   
+                                   // Handle location searching
+                                   $('#place2').keyup(function() {
+                                                      $('#create-event-location-selection').empty();
+                                                      $('#create-event-location-selection').append('<option id="location+1'+
+                                                                                                   '" value="Velg en lokasjon">' +
+                                                                                                   'Velg en lokasjon' +
+                                                                                                   '</option>');
+                                                      searchOnLocation();
+                                                      }).keyup();
+                                   $('#create-event-location-selection').selectmenu();
+                                   
+                                   $('#create-event-location-selection').change(function() {
+                                                                                
+                                                                                // Extract the current index of the location
+                                                                                currentLocationIndex = /location-(\d+)/.exec(
+                                                                                                                             $('#create-event-location-selection option:selected').attr('id'))[1];
+                                                                                });
+                                   
+                                   $('#createeventsubmit').click(function() {
+                                                                 
+                                                                 restClient.create(
+                                                                                   'events/',
+                                                                                   getDataFromCreateForm(),
+                                                                                   function(data, textStatus, jqXHR) {
+                                                                                   $.mobile.hidePageLoadingMsg();
+                                                                                   if (jqXHR.status == 201) {
+                                                                                   showMsg('Event opprettet', function() {
+                                                                                           history.back();
+                                                                                           });
+                                                                                   } else {
+                                                                                   showErr('Noe gikk galt', null);
+                                                                                   }
+                                                                                   }
+                                                                                   ); 
+                                                                 
+                                                                 $('form').die('submit');
+                                                                 return false;
+                                                                 });
+                                   });
 
 function getDataFromCreateForm() {
     var json = '{' +
