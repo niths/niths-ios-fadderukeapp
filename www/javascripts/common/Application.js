6210a226a5f74d2759219ec0cbea3beeaf3cb401
Andre Kristenen <andre.kristensen@lyse.net>
2012-05-08 16:24:35 +0200

    temp

---
+++ www/javascripts/common/Application.js
@@ -0,0 +1,17 @@
+address = 'http://ec2-46-137-46-84.eu-west-1.compute.amazonaws.com:8080/niths/';
+
+student = {};
+
+sessionToken = '';
+
+
+
+applicationKey = 'NH3E5WANTJ';
+applicationToken = 'JJbOUVRgFzOvGGUicKnuhB+fXeyvEnJYpl514ar3nU91brUX1EgisRWwEdj1Byw3CeKjPSnqSSO70xwxmupwjw==';
+developerToken = 'Q9WduujDMII1vTqpnoefwSx7HWkuqOmtdure1g8BCWA1EGN6nPCNQj4KjtuYwIA0j1CnoC5OQE5UvAfq2tFylw==';
+developerKey = 'sob8gvpyk2';
+
+
+htmlEncode = function(s) {
+    return decodeURIComponent(s);
+}
