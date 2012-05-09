615a63d9a185bda8b035e7521ceb9d1bc8210866
Andre Kristenen <andre.kristensen@lyse.net>
2012-05-09 10:02:00 +0200

    orginized lib files

--- www/javascripts/external/ChildBrowser.js
+++ www/javascripts/external/ChildBrowser.js
@@ -5,25 +5,25 @@
 
 function ChildBrowser()
 {
-// Does nothing
+	// Does nothing
 }
 // Callback when the location of the page changes
 // called from native
 ChildBrowser._onLocationChange = function(newLoc)
 {
-window.plugins.childBrowser.onLocationChange(newLoc);
+	window.plugins.childBrowser.onLocationChange(newLoc);
 };
 // Callback when the user chooses the 'Done' button
 // called from native
 ChildBrowser._onClose = function()
 {
-window.plugins.childBrowser.onClose();
+	window.plugins.childBrowser.onClose();
 };
 // Callback when the user chooses the 'open in Safari' button
 // called from native
 ChildBrowser._onOpenExternal = function()
 {
-window.plugins.childBrowser.onOpenExternal();
+	window.plugins.childBrowser.onOpenExternal();
 };
 // Pages loaded into the ChildBrowser can execute callback scripts, so be careful to
 // check location, and make sure it is a location you trust.
@@ -31,8 +31,8 @@ window.plugins.childBrowser.onOpenExternal();
 // called from native
 ChildBrowser._onJSCallback = function(js,loc)
 {
-// Not Implemented
-window.plugins.childBrowser.onJSCallback(js,loc);
+	// Not Implemented
+	window.plugins.childBrowser.onJSCallback(js,loc);
 };
 
 /* The interface that you will use to access functionality */
@@ -40,31 +40,31 @@ window.plugins.childBrowser.onJSCallback(js,loc);
 // Show a webpage, will result in a callback to onLocationChange
 ChildBrowser.prototype.showWebPage = function(loc)
 {
-cordova.exec("ChildBrowserCommand.showWebPage", loc);
+	cordova.exec("ChildBrowserCommand.showWebPage", loc);
 };
 ChildBrowser.prototype.getPage = function(loc){
-cordova.exec("ChildBrowserCommand.getPage",loc);
+	cordova.exec("ChildBrowserCommand.getPage",loc);
 }
 // close the browser, will NOT result in close callback
 ChildBrowser.prototype.close = function()
 {
-cordova.exec("ChildBrowserCommand.close");
+	cordova.exec("ChildBrowserCommand.close");
 };
 // Not Implemented
 ChildBrowser.prototype.jsExec = function(jsString)
 {
-// Not Implemented!!
-//Cordova.exec("ChildBrowserCommand.jsExec",jsString);
+    // Not Implemented!!
+    //Cordova.exec("ChildBrowserCommand.jsExec",jsString);
 };
 // Note: this plugin does NOT install itself, call this method some time after deviceready to install it
 // it will be returned, and also available globally from window.plugins.childBrowser
 ChildBrowser.install = function()
 {
-if(!window.plugins)
-{
-window.plugins = {
-};
-}
-window.plugins.childBrowser = new ChildBrowser();
-return window.plugins.childBrowser;
+	if(!window.plugins)
+	{
+		window.plugins = {
+		};
+	}
+	window.plugins.childBrowser = new ChildBrowser();
+	return window.plugins.childBrowser;
 };
